7e5463ad077f05b373fd52c1fe984a498d4154fb
BillyLjm <billy.ljm@gmail.com>
2025-07-19 17:46:49 +0800

    0176-Second_Highest_Salary

---
+++ 0176-Second_Highest_Salary.sql
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * 0176-Second_Highest_Salary
+ * Billy.Ljm
+ * 19 July 2025
+ *
+ * =======
+ * Problem
+ * =======
+ * https://leetcode.com/problems/second-highest-salary/
+ *
+ * Table: Employee
+ * +-------------+------+
+ * | Column Name | Type |
+ * +-------------+------+
+ * | id          | int  |
+ * | salary      | int  |
+ * +-------------+------+
+ * id is the primary key (column with unique values) for this table.
+ * Each row of this table contains information about the salary of an employee.
+ *  
+ * 
+ * Write a solution to find the second highest distinct salary from the Employee 
+ * table. If there is no second highest salary, return null.
+ *
+ * ===========
+ * My Approach
+ * ===========
+ * We use MAX since it is faster than sorting (and selecting the 2nd highest 
+ * salary afterwards)
+ ******************************************************************************/
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary != (SELECT MAX(salary) FROM Employee)
